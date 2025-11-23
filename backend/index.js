7b19a80792c6e79a748af50b3455b5a7e7d1141f
xubo16 <bo15964008359@outlook.com>
2025-11-23 18:59:02 -0500

    Log allowed origin for debugging

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,13 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import routes from "./routes.js";
-// TODO: complete me (loading the necessary packages)
+
 dotenv.config();
 
 const app = express();
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+console.log("Allowed origin:", FRONTEND_URL);
 
-// TODO: complete me (CORS)
 app.use(cors({
     origin: FRONTEND_URL,
 }));
